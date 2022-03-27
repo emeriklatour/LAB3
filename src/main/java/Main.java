a2e4534ab621ce6f31cda891e82fb9794c6fb182
Emerik <emeriklatour@gmail.com>
2022-03-27 00:13:40 -0400

    Ajout du patron template

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import Initializer.DefaultViewInitializer;
+import Initializer.ViewInitializer;
 import command.Command;
 import display.FenetrePrincipale;
 import mvc.Controller;
@@ -15,49 +17,7 @@ import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) {
-		Modele modele = getDefaultModele();
-		createVue(modele);
-		Controller.getInstance().handleCommand(getInitCommand(modele));
-	}
-
-	private static Modele getDefaultModele() {
-		Map<Side, Perspective> perspectives = new HashMap<>();
-		Arrays.stream(Side.values()).forEach(
-			(s) -> perspectives.put(s, new Perspective())
-		);
-		return new Modele(new Image(), perspectives);
-	}
-
-	private static void createVue(Modele modele) {
-		FenetrePrincipale fenetrePrincipale = new FenetrePrincipale();
-		fenetrePrincipale.addToPanneau(new Vignette(modele.getImage()));
-		Arrays.stream(Side.values()).forEach(
-				(s) -> fenetrePrincipale.addToPanneau(
-						new Panneau(
-							s,
-							modele.getImage(),
-							modele.getPerspective(s)
-						)
-				)
-		);
-	}
-
-	private static Command getInitCommand(Modele modele) {
-		return new Command() {
-			@Override
-			public void execute(Controller controller) {
-				controller.setModele(modele);
-				GestionnaireSauvegarde.getInstance()
-						.registerController(controller);
-			}
-
-			@Override
-			public void revert(Controller controller) {}
-
-			@Override
-			public Side getSide() {
-				return null;
-			}
-		};
+		ViewInitializer initializer = new DefaultViewInitializer();
+		initializer.start();
 	}
 }
